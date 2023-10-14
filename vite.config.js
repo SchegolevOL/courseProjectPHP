073966e5fa8a997b4d18c02d094b27bcddc50c21
Oleg Schegol <olschegolev@yahoo.com>
2023-10-14 22:06:30 +0500

    add css js vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+                'resources/assets/admin/dist/css/adminlte.min.css',
+                'resources/assets/admin/plugins/jquery/jquery.min.js',
+                'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+                'resources/assets/admin/dist/js/adminlte.min.js',
+                'resources/assets/admin/dist/js/demo.js'
+            ],
             refresh: true,
         }),
     ],
